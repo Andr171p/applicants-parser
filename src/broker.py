0905fb929e70f439e1aa9c529e1a72f61d2ee1ea
Andre Medvedev <andrejmedvedev2713@gmail.com>
2025-07-22 13:03:05 +0500

    Добавил функционал работы с брокером и базой данных

--- src/broker.py
+++ src/broker.py
@@ -1,16 +1,24 @@
-from faststream.kafka import KafkaBroker
+from faststream import FastStream
+from faststream.rabbit import RabbitBroker
 
-from .database.session import add_many
-from .schemas import ListApplicantsSchema
+from config import settings
+from database import ListApplicantsSchema, add_many
 
-broker = KafkaBroker()
+broker = RabbitBroker(settings.rabbit_settings.get_rabbit_url)
+app = FastStream(broker)
 
 
-@broker.subscriber(
-    "save_applicants",
-    batch=True,
-    batch_timeout_ms=1000,
-    max_poll_records=1000,
-)
-async def save_applicants(data: ListApplicantsSchema) -> None:
-    await add_many(data=data)
+@broker.subscriber("save_applicants")
+async def save_applicants(data: dict) -> None:
+    applicants = ListApplicantsSchema(**data)
+    await add_many(data=applicants)
+
+
+@app.on_startup
+async def start_app() -> None:
+    await broker.connect()
+
+
+@app.on_shutdown
+async def stop_app() -> None:
+    await broker.stop()
--- src/database/session.py
+++ src/database/session.py
@@ -1,57 +1,24 @@
-import contextlib
 from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
 
-from sqlalchemy.ext.asyncio import (
-    AsyncConnection,
-    AsyncEngine,
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from config import get_db_url
+from config import settings
 
-from ..schemas import ListApplicantsSchema
+from .schemas import ListApplicantsSchema
 
 
-class SQLSessionService:
-    def __init__(self) -> None:
-        self._engine: AsyncEngine | None = None
-        self._sessionmaker: async_sessionmaker[AsyncSession] | None = None
-
-    def init(self) -> None:
-        self._engine = create_async_engine(url=get_db_url(), echo=True)
-        self._sessionmaker = async_sessionmaker(bind=self._engine, expire_on_commit=False)
-
-    async def close(self) -> None:
-        if self._engine is None:
-            return
-        await self._engine.dispose()
-        self._engine = None
-        self._sessionmaker = None
-
-    @contextlib.asynccontextmanager
-    async def session(self):
-        async with self._sessionmaker() as session:
-            try:
-                yield session
-            except Exception as _ex:
-                await session.rollback()
-                raise _ex
-
-    @contextlib.asynccontextmanager
-    async def connect(self) -> AsyncIterator[AsyncConnection]:
-        if self._engine is None:
-            raise OSError("DatabaseSessionManager is not initialized")
-        async with self._engine.begin() as connection:
-            try:
-                yield connection
-            except Exception as _ex:
-                await connection.rollback()
-                raise _ex
+@asynccontextmanager
+async def get_session() -> AsyncIterator[AsyncSession]:
+    engine = create_async_engine(url=settings.sql_settings.get_db_url, echo=True)
+    session_maker = async_sessionmaker(
+        engine, class_=AsyncSession, autoflush=False, expire_on_commit=False
+    )
+    async with session_maker() as session:
+        yield session
 
 
 async def add_many(data: ListApplicantsSchema) -> None:
-    async with SQLSessionService().session() as session:
-        await session.add_all(data)
+    async with get_session() as session:
+        session.add_all(data.to_database())
         await session.commit()
