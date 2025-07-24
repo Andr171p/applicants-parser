46abb627fe1aff758630a466d7a74af5fc3940fe
Andre Medvedev <andrejmedvedev2713@gmail.com>
2025-07-24 15:46:43 +0500

    Протестировал работу  базы данных и брокера

--- src/core/schemas.py
+++ src/core/schemas.py
@@ -9,41 +9,44 @@ from .enums import EducationForm, Source, Submit
 
 class UniversitySchema(BaseModel):
     """Университет"""
-    id: int         # Уникальный ID университета
-    title: str      # Название университета
+
+    id: int  # Уникальный ID университета
+    title: str  # Название университета
     source: Source  # Источник откуда получен университет
-    url: str        # URL университета (источника)
+    url: str  # URL университета (источника)
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class DirectionSchema(BaseModel):
     """Направление подготовки"""
-    university_id: int             # ID университета
-    code: str                      # Код направления подготовки
-    title: str | None              # Название направления подготовки
+
+    university_id: int  # ID университета
+    code: str  # Код направления подготовки
+    title: str | None  # Название направления подготовки
     education_form: EducationForm  # Форма обучения
-    institute: str | None          # Институт
-    budget_places: int             # Количество бюджетных мест
-    total_places: int              # Всего мест
-    education_price: float         # Цена на обучения
+    institute: str | None  # Институт
+    budget_places: int  # Количество бюджетных мест
+    total_places: int  # Всего мест
+    education_price: float  # Цена на обучения
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class ApplicantSchema(BaseModel):
     """Абитуриент из конкурсного списка"""
-    university_id: int      # ID университета
-    direction_code: str     # Код направления подготовки
-    id: int                 # ID абитуриента с Госуслуг
-    serial_number: int      # Порядковый номер
-    priority: int           # Приоритет
-    submit: Submit          # Согласие
-    total_points: int       # Сумма баллов
-    points: list[int]       # Баллы за ВИ
+
+    university_id: int  # ID университета
+    direction_code: str  # Код направления подготовки
+    id: int  # ID абитуриента с Госуслуг
+    serial_number: int  # Порядковый номер
+    priority: int  # Приоритет
+    submit: Submit  # Согласие
+    total_points: int  # Сумма баллов
+    points: list[int]  # Баллы за ВИ
     additional_points: int  # Дополнительные баллы
-    original: bool          # Сдан ли оригинал
-    status: str             # Статус заявления
-    date: datetime          # Дата подачи заявления
+    original: bool  # Сдан ли оригинал
+    status: str  # Статус заявления
+    date: datetime  # Дата подачи заявления
 
     model_config = ConfigDict(from_attributes=True)
--- src/database/database_configs.py
+++ src/database/database_configs.py
@@ -3,16 +3,17 @@ from typing import Annotated
 import uuid
 from datetime import datetime
 
-from sqlalchemy import BIGINT, func
+from sqlalchemy import ARRAY, BIGINT, Integer, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 created_at = Annotated[datetime, mapped_column(server_default=func.now())]
 int_pk = Annotated[int, mapped_column(primary_key=True)]
 uuid_pk = Annotated[
     uuid.UUID, mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 ]
+list_int = Annotated[list[int], mapped_column(ARRAY(Integer), nullable=False)]
 bool_null = Annotated[bool, mapped_column(nullable=False)]
 int_null = Annotated[int, mapped_column(nullable=False)]
 float_null = Annotated[float, mapped_column(nullable=False)]
@@ -26,5 +27,3 @@ str_def = Annotated[str, mapped_column(default=None)]
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
-
-    id: Mapped[int_pk]
